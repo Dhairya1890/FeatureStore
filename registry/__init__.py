4112db2bee54205318d5414aa6499bb2a98c1deb
Dhairya <sharmajikaemail123@gmail.com>
2026-08-15 23:56:42 +0530

    feat: add feature registry

---
+++ registry/__init__.py
@@ -0,0 +1,49 @@
+"""
+Feature Registry
+
+- It is the source of truth for every feature in the system
+- It defines a feature once
+- It stores its metadata
+- It makes the feature discoverable
+- It lets both training and serving code read the same defination
+"""
+
+# Feature Registration API
+
+''' This is a user facing entry point'''
+
+from dataclasses import dataclass
+from typing import Callable
+
+''' What is a dataclass - A Dataclass is a python module that makes it easy to write classes, it automatically handles the init and other class boilerplatting so we don't have to write them'''
+
+''' A dataclass is just a class that holds data, It's python's clean way of defining a container for related fields'''
+
+@dataclass
+class FeatureRecord:
+    name : str
+    entity_type : str
+    fn : Callable
+    ttl : int = 3600
+    description : str = ""
+    data_type : str = "float"
+
+''' In Memory Store '''
+
+_registry : dict[str, FeatureRecord] = {}
+
+
+def feature(entity : str, ttl : int = 3600, description : str = "", data_type : str = "float"):
+
+    def wrapper(fn):
+        new_feature = FeatureRecord(fn.__name__, entity_type=entity, fn=fn, ttl=ttl, description=description, data_type=data_type)
+        _registry[fn.__name__] = new_feature
+        return fn
+    return wrapper
+
+def get(name : str) -> FeatureRecord:
+    return _registry[name]
+
+def list_all() -> dict[str, FeatureRecord]:
+    return _registry
+
